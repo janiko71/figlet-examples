b9ca2a1b858e08b072b5087ff2feacbeb8c7b397
janiko71 <jean@geba.fr>
2021-09-19 19:30:45 +0200

    All fonts added

--- test.py
+++ test.py
@@ -11,9 +11,8 @@ files_list = []
 for file in os.listdir(figlet_path):
     if os.path.isfile(os.path.join(figlet_path, file)):
         ext = os.path.splitext(file)[1]
-        if (ext == '.flf'):
-            filename = os.path.splitext(file)[0].lower()
-            files_list.append(filename)
+        if (ext in ['.flf', '.tlf', '.flc']):
+            files_list.append(file)
 
 # Some alphabetical sorting
 files_list.sort()
@@ -24,6 +23,8 @@ text = "Hello brave new world!"
 with open("output.txt", "w") as output_file:
 
     for font in files_list:
+        font_name = os.path.splitext(font)[0].lower()
+        font_type = os.path.splitext(font)[1].lower()[1:]
         if font[:7] == "obanner":
             statut = "skipped"
         else:
@@ -33,13 +34,13 @@ with open("output.txt", "w") as output_file:
             finally:
                 if (result.returncode == 0):
                     # Everything's fine
-                    output_file.write("Font: {}\n\n".format(font))
+                    output_file.write("Font: {} ({})\n\n".format(font_name, font_type))
                     output_file.write(result.stdout.decode())
                     output_file.write("\n\n")
                     statut = "ok"
                 else:
                     # Houston, we got a problem
                     statut = "file error"
-        print("Font {}: {}".format(font, statut))
+        print("Font {} ({}): {}".format(font_name, font_type, statut))
 
 
